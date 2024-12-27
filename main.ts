9130120719ccafa2a09538c7674e889e5411f5bc
tzwrtzis <tzwrtzis@gmail.com>
2024-12-27 21:50:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+    	
+    }
+})
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+basic.forever(function () {
+	
+})
